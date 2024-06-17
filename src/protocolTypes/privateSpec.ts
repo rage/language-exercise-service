f95b89ab1e93886110c1508fe827290ea6aeb2d2
Henrik Nygren <nygrenh@gmail.com>
2024-06-17 13:12:04 +0300

    Start formulating private spec

--- src/protocolTypes/privateSpec.ts
+++ src/protocolTypes/privateSpec.ts
@@ -1,9 +1,26 @@
-export interface PrivateSpec {
+export type ExerciseType = 'dragging'
+
+export type PrivateSpec = PrivateSpecNoExerciseTypeSelected | PrivateSpecDragging | PrivateSpecTyping 
+
+interface PrivateSpecNoExerciseTypeSelected {
   version: 1
+  exerciseType: null
 }
+
+interface PrivateSpecDragging {
+  version: 1
+  exerciseType: 'dragging'
+}
+
+interface PrivateSpecTyping {
+  version: 1
+  exerciseType: 'typing'
+}
+
 
 export function createEmptyPrivateSpec(): PrivateSpec {
   return {
-    version: 1
+    version: 1,
+    exerciseType: null,
   }
 }
