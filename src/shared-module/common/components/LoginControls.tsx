7915c3688d00b0690d8ea180d3f90876d259757a
Henrik Nygren <nygrenh@gmail.com>
2024-06-27 08:26:41 +0300

    Include shared module in git

---
+++ src/shared-module/common/components/LoginControls.tsx
@@ -0,0 +1,68 @@
+import { ClassNamesArg, cx } from "@emotion/css"
+import { useQueryClient } from "@tanstack/react-query"
+import React, { useContext } from "react"
+import { useTranslation } from "react-i18next"
+
+import LoginStateContext from "../contexts/LoginStateContext"
+import { logout } from "../services/backend/auth"
+import { useCurrentPagePathForReturnTo } from "../utils/redirectBackAfterLoginOrSignup"
+import { loginRoute, signUpRoute } from "../utils/routes"
+
+import Button from "./Button"
+import Spinner from "./Spinner"
+
+export interface LoginControlsProps {
+  styles?: ClassNamesArg[]
+  currentPagePath: string
+}
+
+const LoginControls: React.FC<
+  React.PropsWithChildren<React.PropsWithChildren<LoginControlsProps>>
+> = ({ styles, currentPagePath }) => {
+  const { t } = useTranslation()
+  const loginStateContext = useContext(LoginStateContext)
+  const returnTo = useCurrentPagePathForReturnTo(currentPagePath)
+  const queryClient = useQueryClient()
+
+  if (loginStateContext.isPending) {
+    return <Spinner variant="large" />
+  }
+
+  const submitLogout = async () => {
+    await logout()
+    queryClient.removeQueries()
+    await loginStateContext.refresh()
+    setTimeout(() => {
+      queryClient.refetchQueries()
+    }, 100)
+  }
+
+  return loginStateContext.signedIn ? (
+    <>
+      <li className={cx(styles)}>
+        <Button size="medium" variant="primary" onClick={submitLogout}>
+          {t("log-out")}
+        </Button>
+      </li>
+    </>
+  ) : (
+    <>
+      <li className={cx(styles)}>
+        <a href={signUpRoute(returnTo)}>
+          <Button size="medium" variant="primary">
+            {t("create-new-account")}
+          </Button>
+        </a>
+      </li>
+      <li className={cx(styles)}>
+        <a href={loginRoute(returnTo)}>
+          <Button size="medium" variant="primary">
+            {t("log-in")}
+          </Button>
+        </a>
+      </li>
+    </>
+  )
+}
+
+export default LoginControls
---
+++ src/shared-module/common/components/Navigation/NavBar/SimpleNav.tsx
@@ -0,0 +1,78 @@
+import { css, cx } from "@emotion/css"
+import { useTranslation } from "react-i18next"
+
+import MOOCfi from "../../../img/moocfiLogoNoText.svg"
+import { baseTheme } from "../../../styles"
+import { respondToOrLarger } from "../../../styles/respond"
+import { MARGIN_BETWEEN_NAVBAR_AND_CONTENT } from "../../../utils/constants"
+import SkipLink from "../../SkipLink"
+
+import { NavigationProps } from "."
+
+const StyledIcon = css`
+  font-size: 1.8rem;
+  transform: scale(0.7);
+
+  path {
+    fill: ${baseTheme.colors.gray[600]} !important;
+  }
+`
+const Navbar = css`
+  height: 90px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1rem;
+  padding: 0 1.4rem;
+  margin-bottom: ${MARGIN_BETWEEN_NAVBAR_AND_CONTENT};
+  border-bottom: 2px solid ${baseTheme.colors.gray[100]};
+
+  &:focus-visible {
+    outline: 2px solid ${baseTheme.colors.green[500]};
+    outline-offset: 2px;
+  }
+
+  ${respondToOrLarger.md} {
+    padding: 0 4rem;
+  }
+`
+const NavbarLogo = css`
+  cursor: pointer;
+  color: ${baseTheme.colors.gray[700]};
+
+  & > a:focus-visible {
+    outline: 2px solid ${baseTheme.colors.green[500]};
+    outline-offset: 2px;
+  }
+`
+
+const Navigation: React.FC<React.PropsWithChildren<React.PropsWithChildren<NavigationProps>>> = ({
+  children,
+}) => {
+  const { t, i18n } = useTranslation()
+
+  const makeTopLeftButtonToTemporarilyGoToMoocfi = true
+
+  const moocfiUrl =
+    // eslint-disable-next-line i18next/no-literal-string
+    i18n?.language?.indexOf("fi") !== -1 ? "https://www.mooc.fi" : "https://www.mooc.fi/en"
+
+  return (
+    <nav role="navigation" className={cx(Navbar)} aria-label={t("navigation-menu")}>
+      <SkipLink href="#maincontent">{t("skip-to-content")}</SkipLink>
+      <div className={cx(NavbarLogo)}>
+        <a
+          href={makeTopLeftButtonToTemporarilyGoToMoocfi ? moocfiUrl : "/"}
+          aria-label={t("home-page")}
+          role="button"
+        >
+          <MOOCfi className={cx(StyledIcon)} />
+        </a>
+      </div>
+      {children}
+    </nav>
+  )
+}
+
+export default Navigation
