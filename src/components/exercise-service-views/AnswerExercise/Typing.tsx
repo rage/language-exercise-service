9ef12cd498bdbbc12a5aeab31c6fdb249c2af638
Henrik Nygren <nygrenh@gmail.com>
2024-06-27 07:27:24 +0300

    Implement constructing the answer for typing

--- src/components/exercise-service-views/AnswerExercise/Typing.tsx
+++ src/components/exercise-service-views/AnswerExercise/Typing.tsx
@@ -2,6 +2,7 @@ import useUserAnswerOutputState from "@/hooks/useUserAnswerOutputState"
 import { ExerciseProps } from "."
 import { css } from "@emotion/css"
 import { UserAnswerTyping } from "@/protocolTypes/answer"
+import { ChangeEvent } from "react"
 
 const Typing: React.FC<ExerciseProps> = ({ publicSpec }) => {
   const { selected: answer, updateState: updateAnswer } =
@@ -13,10 +14,21 @@ const Typing: React.FC<ExerciseProps> = ({ publicSpec }) => {
     return null
   }
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>, ItemId: string, nthSlot: number) => {
     const { value } = event.target
 
     event.target.style.width = `${Math.max(20, value.length * 9)}px`
+    updateAnswer((prev) => {
+      if (!prev) {
+        throw new Error("Answer is not initialized")
+      }
+      let itemAnswers = prev.itemAnswers.find((ia) => ia.itemId === ItemId)
+      if (!itemAnswers) {
+        itemAnswers = { itemId: ItemId, answers: [] }
+        prev.itemAnswers.push(itemAnswers)
+      }
+      itemAnswers.answers[nthSlot] = value
+    })
   }
 
   return (
@@ -40,6 +52,7 @@ const Typing: React.FC<ExerciseProps> = ({ publicSpec }) => {
                   return <span key={textPart.text}>{textPart.text.trim()}</span>
                 }
                 placeHolderCounter += 1
+                const nthSlot = placeHolderCounter
 
                 return (
                   <>
@@ -49,7 +62,7 @@ const Typing: React.FC<ExerciseProps> = ({ publicSpec }) => {
                         width: 3rem;
                         min-width: 3rem;
                       `}
-                      onChange={handleInputChange}
+                      onChange={(e) => handleInputChange(e, item.id, nthSlot)}
                       type="text"
                       key={textPart.type + tn}
                     />
