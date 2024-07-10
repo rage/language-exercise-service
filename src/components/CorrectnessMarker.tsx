8219854b2103d9f18bb889b0fc78e509303d9957
Henrik Nygren <nygrenh@gmail.com>
2024-07-10 15:26:02 +0300

    WIP

--- src/components/CorrectnessMarker.tsx
+++ src/components/CorrectnessMarker.tsx
@@ -2,15 +2,18 @@ import { css } from "@emotion/css"
 
 import IncorrectIcon from "@/assets/incorrect.svg"
 import CorrectIcon from "@/assets/correct.svg"
+import { useTranslation } from "react-i18next"
 
 const CorrectnessMarker = ({ isCorrect }: { isCorrect: boolean }) => {
+  const { t } = useTranslation()
+
   return (
     <span
       className={css`
         margin-right: 0.15rem;
       `}
     >
-      {isCorrect ? <CorrectIcon /> : <IncorrectIcon />}
+      {isCorrect ? <CorrectIcon role="img" aria-label={t("correct-slot")} /> : <IncorrectIcon role="img" aria-label={t("incorrect-slot")} />}
     </span>
   )
 }
